f6c345cd1bb493806cca275f0ef8c673d96fd508
elvis <cenelvis@gmail.com>
2022-06-30 13:59:44 -0400

    Wrapping component to the (_app.js) file (root component)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "../components/layout/Layout"; // Wrapping layout here will be added to all our app
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
